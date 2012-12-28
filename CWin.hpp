2378efe4eeda013a50534b0418c0b625cc5fd948
CurlyBrackets <monkaw12@gmail.com>
2012-12-28 00:58:15 -0800

    blub

---
+++ CWin.hpp
@@ -0,0 +1,98 @@
+#ifndef __CWIN_H__
+#define __CWIN_H__
+#ifdef WIN32
+    #include <PDCurses/curses.h>
+#else
+    #include <ncurses.h>
+#endif
+#include <string>
+#include <vector>
+
+#define COLOUR_BLACK       0x0
+#define COLOUR_DARK_BLUE   0x1
+#define COLOUR_DARK_GREEN  0x2
+#define COLOUR_TURQUOISE   0x3
+#define COLOUR_DARK_RED    0x4
+#define COLOUR_PURPLE      0x5
+#define COLOUR_DARK_YELLOW 0x6
+#define COLOUR_LIGHT_GREY  0x7
+#define COLOUR_DARK_GREY   0x8
+#define COLOUR_BLUE        0x9
+#define COLOUR_GREEN       0xA
+#define COLOUR_CYAN        0xB
+#define COLOUR_RED         0xC
+#define COLOUR_MAGENTA     0xD
+#define COLOUR_YELLOW      0xE
+#define COLOUR_WHITE       0xF
+
+
+class CWinStorage;
+
+class CWin{
+    public:
+        CWin(unsigned int sx, unsigned int sy, unsigned int dx, unsigned int dy, bool scrolling = false);
+        ~CWin();
+
+        void mv(unsigned int x, unsigned int y);
+        void print(int ch, unsigned int x = -1, unsigned int y = -1);
+        void print(std::string in, int attr = A_NORMAL, unsigned int x = -1, unsigned int y = -1);
+        void refresh();
+        void clear();
+        void test_border();
+        void horiz_line(unsigned int width, unsigned int x=-1, unsigned int y=-1);
+        int createColor(int FG, int BG);
+        void attrOn(chtype attr);
+        void attrOff(chtype attr);
+
+        unsigned int lastCol() const;//should this be private?
+        void setCol(unsigned int newCol, bool raw = false);
+        WINDOW* win() const;
+        static void start();
+        static void end();
+
+        unsigned int xMax() const;
+        unsigned int yMax() const;
+        unsigned int xPos() const;
+        unsigned int yPos() const;
+
+        int scroll(int n);
+        void saveState();
+        void restoreState();
+
+        std::string input(unsigned int x, unsigned int y, unsigned int dx);
+    private:
+        void defarg(unsigned int& x, unsigned int& y);
+
+        static int colorInc;
+        unsigned int _cX, _cY, _mX, _mY, sX, sY;
+        int _lastCol;
+        bool _scrolling;
+        WINDOW* _win;
+        CWinStorage* dataStore;
+
+        static int key_enter;
+};
+
+class CWinStorage{
+    public:
+        CWinStorage(CWin* window);
+        int getChr(int x, int y) const;
+        int getCol(int x, int y) const;
+        int getCx() const;
+        int getCy() const;
+    private:
+        class Node{
+            public:
+                Node(int chr, int col);
+                int getChr() const;
+                int getCol() const;
+            private:
+                int col, chr;
+        };
+        unsigned int width, height, cx, cy;
+        std::vector<Node> nodes;
+};
+
+
+#endif // __CWIN_H__
+
---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <Curly/CWin.hpp>
+#include <iostream>
+#include <cstdlib>
+
+class Node{
+    public:
+        Node(unsigned int colour):
+        _colour(colour),_link(NULL){}
+        ~Node(){
+            if(next() && !next()->first())
+                delete next();
+        }
+        void link(Node* node){
+            _link = node;
+        }
+        Node* next() const{
+            return _link;
+        }
+
+        unsigned int colour() const{
+            return _colour;
+        }
+        void push(Node* node){
+            if(next()){
+                if(next()->first()){
+                    Node* temp = next();
+                    link(node);
+                    node->link(temp);
+                }
+                else
+                    next()->push(node);
+
+            }
+            else if(first()){
+                node->link(this);
+                link(node);
+            }
+        }
+
+        bool first() const{
+            return _first;
+        }
+        void first(bool value){
+            _first = value;
+        }
+
+    private:
+        unsigned int _colour;
+        bool _first;
+        Node* _link;
+};
+
+int main(){
+    CWin::start();
+    CWin* test = new CWin(1,1,10,10);
+    refresh();
+    Node* first = new Node(test->createColor(0xF, 0x0)), *node = first;
+    first->first(true);
+    first->push(new Node(test->createColor(0xE, 0x0)));
+    first->push(new Node(test->createColor(0xD, 0x0)));
+    first->push(new Node(test->createColor(0xC, 0x0)));
+    first->push(new Node(test->createColor(0xB, 0x0)));
+    first->push(new Node(test->createColor(0xA, 0x0)));
+    first->push(new Node(test->createColor(0x9, 0x0)));
+    first->push(new Node(test->createColor(0x8, 0x0)));
+    first->push(new Node(test->createColor(0x7, 0x0)));
+    first->push(new Node(test->createColor(0x6, 0x0)));
+    first->push(new Node(test->createColor(0x5, 0x0)));
+    first->push(new Node(test->createColor(0x4, 0x0)));
+    first->push(new Node(test->createColor(0x3, 0x0)));
+    first->push(new Node(test->createColor(0x2, 0x0)));
+    first->push(new Node(test->createColor(0x1, 0x0)));
+
+    int chr = 32;
+    for(unsigned int y=0;y<test->yMax();y++){
+        for(unsigned int x=0;x<test->xMax();x++){
+            test->setCol(node->colour());
+            node = node->next();
+            test->print(chr++);
+            if(chr > 127){
+                chr = 32;
+            }
+        }
+    }
+
+    test->refresh();
+    test->saveState();
+    getch();
+    CWin* derp = new CWin(0,0,80,25);
+    refresh();
+    derp->setCol(first->colour());
+    derp->clear();
+    derp->refresh();
+    getch();
+    delete derp;
+    refresh();
+    system("cls");
+    test->restoreState();
+    redrawwin(test->win());
+    test->print('a',0,0);
+    test->refresh();
+    getch();
+    delete test;
+    delete first;
+
+    CWin* win = new CWin(0,0,80,25);
+    refresh();
+    win->print("Something: ");
+    win->refresh();
+    win->input(0, 1, 20);
+
+    delete win;
+
+    CWin::end();
+    return 0;
+}
